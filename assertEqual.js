1d5d5e00150d09b8b6e3c20dbca58de332ee441c
John Kang <hbkang02@gmail.com>
2022-11-14 15:37:34 -0800

    assertEqual.js interpolated

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log('👌🏻👌🏻👌🏻Assertion Passed: ' + actual + ' === ' + expected);
+    console.log(`👌🏻👌🏻👌🏻Assertion Passed: ${actual}  === ${expected}`);
   } else {
-    console.log('💩💩💩Assertion Failed: ' + actual + ' !== ' + expected);
+    console.log(`💩💩💩Assertion Failed: ${actual}  !== ${expected}`);
   }
 };
 
