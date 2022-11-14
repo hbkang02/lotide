aea56bf8cc984863d403728cd908e0edd2617d54
John Kang <hbkang02@gmail.com>
2022-11-14 15:28:07 -0800

    assertEqual.js 1

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,10 @@
 const assertEqual = function(actual, expected) {
-  if (actual === expected){
+  if (actual === expected) {
     console.log('👌🏻👌🏻👌🏻Assertion Passed: ' + actual + ' === ' + expected);
   } else {
     console.log('💩💩💩Assertion Failed: ' + actual + ' !== ' + expected);
-  }};
+  }
+};
 
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
