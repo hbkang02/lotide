1f85961b5169fd01075825f95889c51403eefbdb
John Kang <hbkang02@gmail.com>
2022-11-14 16:53:52 -0800

    tail.js: slice method

---
+++ tail.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👌🏻👌🏻👌🏻Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`💩💩💩Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(array) {
+  return array.slice(1);
+};
+
+
+const words = ["Yo Yo"];
+tail(words);
+assertEqual(words.length, 3);
