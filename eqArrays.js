1758bb4bb961cf89d803fe42c85a460d3bb59182
John Kang <hbkang02@gmail.com>
2022-11-16 22:19:10 -0800

    eqArrays.js review for later

--- eqArrays.js
+++ eqArrays.js
@@ -1,28 +1,31 @@
-const assertEqual = function(actual, expected) {
-  if ((actual === expected)) {
-    return console.log(`👌🏻👌🏻👌🏻Assertion Passed: ${actual}  === ${expected}`);
-  } 
-    return console.log(`💩💩💩Assertion Failed: ${actual}  !== ${expected}`);
-  };
+// eqArrays
 
- 
-  const eqArrays = function(arr1, arr2) {
-    if (arr1.length !== arr2.length) {
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`👌🏻👌🏻👌🏻Assertion Passed: ${actual}  === ${expected}`);
+  } else {
+    console.log(`💩💩💩Assertion Failed: ${actual}  !== ${expected}`);
+  }
+};
+
+// implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
+// use assertEqual to write test cases for various scenarios.
+
+function eqArrays(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    if(arr1[i] !== arr2[i]) {
       return false;
     }
-    for (let i = 0; i < arr1.length; i++) {
-      if (Array.isArray(arr1[i]) && (eqArrays(arr1[i], arr2[i]) === false)) {
-        return false;
-      }
-      if (!(Array.isArray(arr1[i])) && arr1[i] !== arr2[i]) {
-        return false;
-      }
-    } 
-    return true;
-  };
+  }
+  return true;
+};
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
-console.log(eqArrays([1, 2, 3], [1, 2, 3]));
-console.log(eqArrays([1, 2, 3], [3, 2, 1]));
+
+
+//test
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should pass
+console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
+console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
