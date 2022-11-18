d3d211fea36b9b309b293327eab50ca1d8cd4388
John Kang <hbkang02@gmail.com>
2022-11-18 00:50:58 -0800

    takeUntil.js use of cb

--- takeUntil.js
+++ takeUntil.js
@@ -1,27 +1,28 @@
-function assertArraysEqual(arr1, arr2) {
-    if (eqArrays(arr1, arr2)) {
-      console.log(`👌🏻👌🏻👌🏻Assertion Passed: ${arr1}  === ${arr2}`);
+function assertArraysEqual(actual, expected) {
+    if (eqArrays(actual, expected)) {
+      console.log(`👌🏻👌🏻👌🏻Assertion Passed: ${actual}  === ${expected}`);
     } else {
-      console.log(`💩💩💩Assertion Failed: ${arr1}  !== ${arr2}`);
+      console.log(`💩💩💩Assertion Failed: ${actual}  !== ${expected}`);
     }
   };
 
-const eqArrays = function(arr1, arr2) {
-  for (let i = 0; i < arr1.length || i < arr2.length; ++i) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
+  const eqArrays = function(arr1, arr2) {
+    for (let i = 0; i < arr1.length || i < arr2.length; ++i) {
+      if (arr1[i] !== arr2[i]) {
+        return false;
       } 
-  } 
-  return true;
-};
+    } return true;
+  };
+  
+  
   
 // function will return slice of the array with elements taken from the beginning. it should keep going until the callback/predicate returns a truthy value.
 
 const takeUntil = function(array, callback) {
-  const arr1 = [];
+  const results = [];
   for(let arr of array)
-    if(callback === true)
-  return arr1;
+    if(callback(arr) === false) {
+      result.push(arr);
+    }
+  return results;
 };
-
-assertArraysEqual(eqArrays([1, 2, 5, 7, 2, -1, 2, 4, 5], ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"]));
